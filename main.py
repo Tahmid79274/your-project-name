62528b7f89ce60a70116a3f603bf7b3d40812d73
Tahmid79274 <tahmidur.rahman.79274@gmail.com>
2025-12-31 18:56:12 +0600

    added delta times

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        # print(f"dt: {dt}")bootdev run 658641f2-586b-48eb-bfb0-e7612cdd5dfd
 
 
 if __name__ == "__main__":
