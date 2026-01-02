096c567e421200a09bd6ec97b847485ff0590912
Tahmid <tahmiudr.rahman.79274@gmail.com>
2026-01-02 17:03:02 +0600

    palyer model created.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
 import pygame
 
+from player import Player
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
@@ -9,6 +11,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        player.draw(screen)
         # print(f"dt: {dt}")bootdev run 658641f2-586b-48eb-bfb0-e7612cdd5dfd
 
 
