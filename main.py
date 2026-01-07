ab57f9d0a1c6b228e9012313f018a588632d02fc
Tahmid <tahmiudr.rahman.79274@gmail.com>
2026-01-07 16:57:50 +0600

    asteriod is complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from asteroid import Asteroid
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT,LINE_WIDTH
 from logger import log_state
 import pygame
+from asteroidfield import AsteroidField
 
 from player import Player
 
@@ -13,7 +15,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
